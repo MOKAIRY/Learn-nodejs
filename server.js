6a21a217ab8e3ba3e35a47b8c86cb47f824ae804
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-17 12:06:30 +0300

    Use Request object properties

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ import http from "node:http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello From The Server!\n");
-  res.write("This is some Date.\n");
-  res.write("This is more Date.");
-  res.end(() => console.log("A Response Has Been  End."));
+  if (req.url === "/api" && req.method === "GET") {
+    res.write("Request URL: " + req.url + "\n");
+    res.write("Request Method: " + req.method + "\n");
+    res.end("Hello From The Server!", () =>
+      console.log("A Connection Has Been Closed.")
+    );
+  } else {
+    res.end("Sorry, Bad Request", () =>
+      console.log("A Bad Connection Has Been Closed.")
+    );
+  }
 });
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
