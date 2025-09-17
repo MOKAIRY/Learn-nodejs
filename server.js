5a25573779607437f776f7e34b98198a1590b1cc
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-17 11:26:59 +0300

    Modify server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import http from "node:http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello From The Server!");
+  res.write("Hello From The Server!\n");
+  res.write("This is some Date.\n");
+  res.write("This is more Date.");
+  res.end(() => console.log("A Response Has Been  End."));
 });
 
-server.listen(PORT, () => console.log("Server running on port: 8000"));
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
