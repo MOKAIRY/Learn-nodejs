3bd7fcf0a818b9a37d325cc57a5e88279ca0d92d
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-17 20:54:14 +0300

    Try Json

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-import http from "node:http";
+const animal = {
+  type: "cattle",
+  name: "Yak",
+};
 
-const PORT = 8000;
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/api" && req.method === "GET") {
-    res.write("Request URL: " + req.url + "\n");
-    res.write("Request Method: " + req.method + "\n");
-    res.end("Hello From The Server!", () =>
-      console.log("A Connection Has Been Closed.")
-    );
-  } else {
-    res.end("Sorry, Bad Request", () =>
-      console.log("A Bad Connection Has Been Closed.")
-    );
-  }
-});
-
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+console.log(JSON.stringify(animal));
+console.log(typeof JSON.stringify(animal));
