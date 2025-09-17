46c8ab0d23c25b9bda5db20a4fa6e76dce0aebf1
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-17 21:06:22 +0300

    Revert "v1.1"

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const animal = {
-  type: "cattle",
-  name: "Yak",
-};
+import http from "node:http";
 
-console.log(JSON.stringify(animal));
-console.log(typeof JSON.stringify(animal));
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.write("Hello From The Server!\n");
+  res.write("This is some Date.\n");
+  res.write("This is more Date.");
+  res.end(() => console.log("A Response Has Been  End."));
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
