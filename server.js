8f41e792669fa421a77c9a56a12a2351c59e45f1
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-17 21:55:32 +0300

    Dealing with Json & Data

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  res.write("Hello From The Server!\n");
-  res.write("This is some Date.\n");
-  res.write("This is more Date.");
-  res.end(() => console.log("A Response Has Been  End."));
+const server = http.createServer(async (req, res) => {
+  const destination = await getDataFromDB();
+
+  if (req.url === "/api" && req.method === "GET") {
+    res.write("This is from the server\n");
+    res.write("Request URL: " + req.url + "\n");
+    res.write("Request Method: " + req.method + "\n");
+    res.write("Request Status Code: " + req.statusCode + "\n");
+    res.write("Requested Data:\n");
+    res.end(JSON.stringify(destination), () => {
+      console.log("A Request Has Been Ended");
+    });
+  } else {
+    res.end("Bad URL", () => {
+      console.log("A Bad Request Has Been Ended");
+    });
+  }
 });
 
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
